a4f7f2856de7ec8d6036a491260eeb6ba9063fa3
Simon <simon.tannai@keyrus.com>
2018-07-02 14:49:11 +0200

    try to debug into the module

--- src/utils/banMiddleware.js
+++ src/utils/banMiddleware.js
@@ -1,8 +1,8 @@
 module.exports = async (context, next, done) => {
   const offenseLevel = await context.brain.conversationGet(context.user, 'offenseLevel')
+  console.log(`offenseLevel: ${offenseLevel}`)
 
   if (offenseLevel >= context.config.offenseLimit) {
-    console.log(`offenseLevel: ${offenseLevel}`)
     console.log(`Bot muted for user ${context.userMessage.user}`)
 
     await done()

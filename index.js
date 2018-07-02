c07e0d54d1e0c23f16f8e7c8d825e7dc6ff5025d
Simon <simon.tannai@keyrus.com>
2018-07-02 16:20:13 +0200

    Fix issue import on index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ComplimentDialog = require(path.join(__dirname, 'src', 'dialogs', 'complim
 const GoodbyeDialog = require(path.join(__dirname, 'src', 'dialogs', 'goodbye-dialog.js'))
 const GreetingDialog = require(path.join(__dirname, 'src', 'dialogs', 'greeting-dialog.js'))
 const HelpDialog = require(path.join(__dirname, 'src', 'dialogs', 'help-dialog.js'))
-const OffenseDialog = require(path.join(__dirname, 'src', 'dialogs', 'offense-dialog.js'))
+const OffenseSmalltalksDialog = require(path.join(__dirname, 'src', 'dialogs', 'offense-dialog.js'))
 const RealOrNotDialog = require(path.join(__dirname, 'src', 'dialogs', 'realornot-dialog.js'))
 const RestartDialog = require(path.join(__dirname, 'src', 'dialogs', 'restart-dialog.js'))
 const ThanksDialog = require(path.join(__dirname, 'src', 'dialogs', 'thanks-dialog.js'))
@@ -26,7 +26,7 @@ module.exports = {
   GoodbyeDialog,
   GreetingDialog,
   HelpDialog,
-  OffenseDialog,
+  OffenseSmalltalksDialog,
   RealOrNotDialog,
   RestartDialog,
   ThanksDialog,
